7f3419b8ff7174e537048858a5c3171574c615a7
李志鹏 <lzp@Mac.local>
2017-02-08 14:24:55 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule,AccordionModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    AccordionModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
